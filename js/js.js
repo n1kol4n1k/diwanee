ddddd98017ac00561547a1e6f6573f6dd7a84ea6
Ilic <n1kol4n1k@gmail.com>
2017-07-28 17:07:15 +0200

    Update1

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function opennavi() {
+	if(document.getElementById('headeroverlay').style.display == "none")
+	{
+  		document.getElementById('headeroverlay').style.display = "block";
+	}
+	else {
+		document.getElementById('headeroverlay').style.display = "none";
+	}
+}// JavaScript Document
